3fc51059322410b6579ac9d76d74ce336c2085b4
Shideshan <307042873@qq.com>
2019-12-13 09:24:38 +0800

    init

--- src/main.js
+++ src/main.js
@@ -2,6 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import 'element-ui/lib/theme-chalk/index.css'
+import '~assets/css/icon.css'
+import '~plugins/index.js'
+import './router/guard'
+import 'babel-polyfill'
 
 Vue.config.productionTip = false
 
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,23 @@
+const path = require('path')
+
+const aliasMap = {
+  '~api': 'src/api',
+  '~assets': 'src/assets',
+  '~components': 'src/components',
+  '~utils': 'src/utils',
+  '~plugins': 'src/plugins',
+  '~views': 'src/views'
+}
+
+function resolve (dir) {
+  return path.join(__dirname, dir)
+}
+
 module.exports = {
-  lintOnSave: false
+  lintOnSave: false,
+  chainWebpack: config => {
+    Object.keys(aliasMap).forEach(key => {
+      config.resolve.alias.set(key, resolve(aliasMap[key]))
+    })
+  }
 }
